33b7438b03a77afe2a90f85f39d7786b804e1e17
Aman Gupta <aman@tmm1.net>
2020-07-16 15:58:42 -0700

    try changing loader address for x64

--- src/arch.h
+++ src/arch.h
@@ -77,7 +77,7 @@ typedef unsigned char byte_t;
     #define OFFSETOF_STAT_UID_32 24
     #define OFFSETOF_STAT_GID_32 28
 
-    #define LOADER_ADDRESS 0x600000000000
+    #define LOADER_ADDRESS 0x500000000000
     #define HAS_LOADER_32BIT true
 
     #define EXEC_PIC_ADDRESS   0x500000000000
